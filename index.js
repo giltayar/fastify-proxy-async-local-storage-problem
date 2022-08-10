a97b6205ef4192c783f610ecc1ec7d2cc48c3fdd
Gil Tayar <gil@tayar.org>
2022-08-10 21:35:32 +0300

    add POST and 404

--- index.js
+++ index.js
@@ -3,55 +3,60 @@ import fastifyHttpProxy from "@fastify/http-proxy";
 import { deepEqual as assertEqual } from "assert/strict";
 import { fastifyRequestContextPlugin } from "@fastify/request-context";
 import { setTimeout } from "timers/promises";
-import {AsyncLocalStorage} from 'node:async_hooks'
+import { AsyncLocalStorage } from "node:async_hooks";
 import throat from "throat";
 
 const store = new AsyncLocalStorage();
 
 // The target of the Proxy
 const targetInstance = fastify();
-targetInstance.get("/", () => setTimeout(1500).then(() => "It works!"));
+targetInstance.get("/", () => setTimeout(300).then(() => "It works!"));
+targetInstance.get("/404", (_req, reply) =>
+  setTimeout(1200).then(() => {
+    reply.status(404).send("It 404!");
+  })
+);
 const targetBaseUrl = await targetInstance.listen();
 
 // The app that should be causing the problem, and has
 // a regular endpoint and a proxy endpoint to the target app above.
 const instance = fastify();
 // instance.register(fastifyRequestContextPlugin);
-instance.addHook('onRequest', (req, reply, next) => {
-  store.run(new Map(), () => next())
-})
+instance.addHook("onRequest", (req, reply, next) => {
+  store.run(new Map(), () => next());
+});
 
 instance.register(fastifyHttpProxy, {
   prefix: "/api",
   upstream: targetBaseUrl,
   target: "http://localhost:3001",
 });
-instance.get("/result", async (req) => {
+instance.post("/result", async (req) => {
   const { time, result } = req.query;
 
-  let requestContext = store.getStore()
+  let requestContext = store.getStore();
   requestContext.set("time", parseInt(time));
   requestContext.set("result", result);
 
   await waitAndResult(req);
 
-  requestContext = store.getStore()
+  requestContext = store.getStore();
 
-  return requestContext.get('finalResult')
+  return requestContext.get("finalResult");
 });
 
 async function waitAndResult(req) {
-  const requestContext = store.getStore()
+  const requestContext = store.getStore();
 
   await setTimeout(requestContext.get("time"));
 
   const result = requestContext.get("result");
 
-  await 1
+  await 1;
 
-  requestContext.set('finalResult', result + '!')
+  requestContext.set("finalResult", result + "!");
 
-  await 1
+  await 1;
 }
 
 const baseUrl = await instance.listen();
@@ -64,7 +69,7 @@ assertEqual(
   "It works!"
 );
 assertEqual(
-  await fetch(new URL("/result?time=100&result=hello", baseUrl)).then((x) =>
+  await fetch(new URL("/result?time=100&result=hello", baseUrl), {method: 'POST'}).then((x) =>
     x.text()
   ),
   "hello!"
@@ -80,17 +85,26 @@ await Promise.all(
         const result = (Math.random() * 1000) | 0;
 
         const fetchResult = await fetch(
-          new URL(`/result?time=${time}&result=${result}`, baseUrl)
+          new URL(`/result?time=${time}&result=${result}`, baseUrl),
+          {
+            method: "POST",
+            body: "assssssassssssassssssassssssassssssassssssassssssassssssassssss",
+          }
         ).then((x) => x.text());
 
         assertEqual(
           fetchResult,
-          result.toString() + '!',
+          result.toString() + "!",
           `${i} - ${time} - ${result} != ${fetchResult}`
         );
         assertEqual(
-          await fetch(new URL("/api", baseUrl)).then((x) => x.text()),
-          "It works!"
+          (
+            await Promise.all([
+              fetch(new URL("/api/", baseUrl)).then((x) => x.text()),
+              fetch(new URL(`/api/404`, baseUrl)).then((x) => x.text()),
+            ])
+          ).join(","),
+          "It works!,It 404!",
         );
         process.stdout.write(".");
       })
